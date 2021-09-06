0a9f7d4538f943bfa88384c8c39075237b6afc09
Alejandro Ariel Moran <alejandroariel.moran9@gmail.com>
2021-09-06 14:20:54 +0000

    fixed object-fit and margin

--- src/js/component/cards.js
+++ src/js/component/cards.js
@@ -3,31 +3,16 @@ import PropTypes from "prop-types";
 
 export const Cards = props => {
 	return (
-		<div className="col-md-3">
+		<div className="col-md-3 my-3">
 			<div className="card shadow-sm">
-				<svg
-					className="bd-placeholder-img card-img-top"
-					width="100%"
-					height="225"
-					xmlns="http://www.w3.org/2000/svg"
-					role="img"
-					aria-label="Placeholder: Thumbnail"
-					preserveAspectRatio="xMidYMid slice"
-					focusable="false">
-					<title>Placeholder</title>
-					<rect width="100%" height="100%" fill="#55595c"></rect>
-					<text x="50%" y="50%" fill="#eceeef" dy=".3em">
-						Thumbnail
-					</text>
-				</svg>
-
+				<img src={props.img} className="img-fluid card-img" />
 				<div className="card-body">
 					<p className="card-text">
 						This is a wider card with supporting text below as a
 						natural lead-in to additional content. This content is a
 						little bit longer.
 					</p>
-					<h1>{props.title}</h1>
+					<h4>{props.title}</h4>
 
 					<div className="d-flex justify-content-between align-items-center">
 						<div className="btn-group">
@@ -51,5 +36,6 @@ export const Cards = props => {
 };
 
 Cards.propTypes = {
-	title: PropTypes.string
+	title: PropTypes.string,
+	img: PropTypes.string
 };
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 import { Navbar } from "./navbar.js";
 import { Jumbotron } from "./jumbotron";
 import { Cards } from "./cards";
@@ -9,7 +9,27 @@ import { Cards } from "./cards";
 export function Home() {
 	// const [arr, setArr] = useState([]);
 	// const [newStr, setNewStr] = useState("");
-	let arr = ["", "", "", ""];
+	let arr = [
+		{
+			name: "Libertadores",
+			img: "https://www.conmebol.com/sites/default/files/000_1bh0dn.jpg"
+		},
+		{
+			name: "Sudamericana",
+			img:
+				"https://images.daznservices.com/di/library/Goal_Colombia/24/8a/river-plate-copa-sudamericana-2014_1j4v8ry4krpos1po2p4v5ffdul.jpg?t=1773037457&quality=100"
+		},
+		{
+			name: "Suruga Bank",
+			img:
+				"https://i.pinimg.com/originals/16/9b/71/169b71c0c3b5a962e48315d7409b10a8.jpg"
+		},
+		{
+			name: "Recopa",
+			img:
+				"https://t.resfu.com/media/img_news/river-celebrando-la-recopa-sudamericana-2016--afp.jpg"
+		}
+	];
 	return (
 		<div className="text-center">
 			<Navbar />
@@ -25,15 +45,14 @@ export function Home() {
 				}}>
 				skdjg
 			</button> */}
-			<div className="container">
+			<div className="container ">
 				<div className="row">
 					{arr.map((el, i) => {
 						console.log(el, i);
-						return <Cards title={el} key={i} />;
+						return <Cards title={el.name} key={i} img={el.img} />;
 					})}
 				</div>
 			</div>
-			;
 		</div>
 	);
 }
